9df2da38b81acb535ad0703eac6613be06598b81
Chris Petersen <geek+github@ex-nerd.com>
2012-01-05 23:08:39 -0800

    tiny version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='cpool',
-    version='0.1',
+    version='0.1.1',
     author='Chris Petersen',
     author_email='geek@ex-nerd.com',
     packages=['cpool'],
